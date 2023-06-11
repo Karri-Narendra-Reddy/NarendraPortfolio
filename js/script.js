085169866b9ea1ba4d1bd85ac0702b94b05b654d
Karri-Narendra-Reddy <narendrareddykarri11@gmail.com>
2023-06-11 20:57:03 +0530

    Latest

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function displayAlert() {
+    alert('Still Under Development');
+  }
+
+  function displayAlert1() {
+    alert('Sorry We Cannot Process Your Request');
+    alert('Still Under Development');
+  }
