597916da777a2eb04a32ef651639e4a0b69c3ff7
Karri-Narendra-Reddy <narendrareddykarri11@gmail.com>
2023-06-13 22:42:23 +0530

    latest changes on 13062023

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
+function calculateAge() {
+  var birthYear = 2002; // Replace with your birth year
+  var currentYear = new Date().getFullYear();
+  var age = currentYear - birthYear;
+  document.getElementById("age").innerText = age;
+}
+
+window.onload = function() {
+  calculateAge();
+};
+
 function displayAlert() {
     alert('Still Under Development');
   }
 
-  function displayAlert1() {
-    alert('Sorry We Cannot Process Your Request');
-    alert('Still Under Development');
-  }
+function displayAlert1() {
+  alert('Sorry We Cannot Process Your Request');
+  alert('Still Under Development');
+}
+
+
+
+// Automatically update age when the page loads
+
+
